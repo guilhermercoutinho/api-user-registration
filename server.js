d80ed2e54f24fbf895545ecfcf3c78a06b5e33ba
guilhermercoutinho <guilhermecoutinho.rc@gmail.com>
2024-09-13 13:58:27 -0300

    Configuração de cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const prisma = new PrismaClient();
 
